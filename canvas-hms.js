ed02ef388518003a08244d1b8c66e930aa9fbfe7
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-10-01 20:03:58 +0100

    Added write, slope, setTemperature methods

--- canvas-hms.js
+++ canvas-hms.js
@@ -21,43 +21,83 @@ class HMS {
             Object.keys(options).forEach(key => this.options[key] = options[key]);
     }
 
-    raise({peak, amount}) {
-        const {context, canvas, options: {curveWidth}} = this;
-        const {width, height} = canvas;
-        const image = context.getImageData(0, 0, width, height);
-        const {data} = image;
+    write(cb, {x = 0, y = 0, width = this.canvas.width, height = this.canvas.height}) {
+        const imageData = this.context.getImageData(x, y, width, height);
+        const pixels = imageData.data;
 
-        let r, g, b, sum, op, index = 0;
+        let i, r, g, b, a, R, G, B, A;
+        for(i = 0; i < pixels.length; i += 4) {
+            r = pixels[i];
+            g = pixels[i + 1];
+            b = pixels[i + 2];
+            a = pixels[i + 3];
 
-        for(index; index < data.length; index += 4) {
-            r = data[index];
-            g = data[index + 1];
-            b = data[index + 2];
+            // Passively get result
+            [R = r, G = g, B = b, A = a] = cb({r, g, b, a});
 
+            // Set result
+            pixels[i] = R;
+            pixels[i + 1] = G;
+            pixels[i + 2] = B;
+            pixels[i + 3] = A;
+        }
+
+        this.context.putImageData(imageData, x, y);
+    }
+
+    // -50 to 50 color slope
+    slope({low, high, amount}) {
+        let deltaR = high.r - low.r,
+            deltaG = high.g - low.g,
+            deltaB = high.b - low.b;
+
+        this.write(({r, g, b}) => {
+            return [
+                r + (deltaR * (amount / 100)),
+                g + (deltaG * (amount / 100)),
+                b + (deltaB * (amount / 100))
+            ];
+        }, {});
+    }
+
+    distort({peak, amount}) {
+        const {options: {curveWidth}} = this;
+
+        let sum, increase;
+
+        this.write(({r, g, b}) => {
             sum = r + g + b;
 
             if((sum > (peak - (curveWidth / 2))) && (sum < (peak + (curveWidth / 2)))) {
-                op = amount * curve(sum - peak, curveWidth);
+                increase = amount * curve(sum - peak, curveWidth);
 
-                data[index] = clamp(r + op, 0, 255);
-                data[index + 1] = clamp(g + op, 0, 255);
-                data[index + 2] = clamp(b + op, 0, 255);
+                r = clamp(r + increase, 0, 255);
+                g = clamp(g + increase, 0, 255);
+                b = clamp(b + increase, 0, 255);
             }
-        }
 
-        context.putImageData(image, 0, 0);
+            return [r, g, b];
+        }, {});
     }
 
     setHighlights(amount, maxSum = this.options.maxSum) {
-        this.raise({peak: maxSum / (4 / 3), amount}); 
+        this.distort({peak: maxSum / (4 / 3), amount}); 
     }
 
     setMidtones(amount, maxSum = this.options.maxSum) {
-        this.raise({peak: maxSum / 2, amount}); 
+        this.distort({peak: maxSum / 2, amount}); 
     }
     
     setShadows(amount, maxSum = this.options.maxSum) {
-        this.raise({peak: maxSum / 4, amount}); 
+        this.distort({peak: maxSum / 4, amount}); 
+    }
+
+    setTemperature(amount) {
+        this.slope({
+            low:  {r: 0, g: 50, b: 255},
+            high: {r: 255, g: 50, b: 0},
+            amount
+        });
     }
 }
 
